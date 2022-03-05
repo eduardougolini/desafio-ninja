ec670bf67689fdbccbad34c27726a99805431512
Eduardo Ugolini <edu.ugolini2@gmail.com>
2022-03-05 20:31:27 -0300

    Add error handlers for the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
+# Base controller for the project
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::ParameterMissing, with: :missing_param
+
+  def not_found
+    head :not_found
+  end
+
+  def missing_param
+    render json: { error: I18n.t('errors.messages.missing_param') }, status: :unprocessable_entity
+  end
 end
