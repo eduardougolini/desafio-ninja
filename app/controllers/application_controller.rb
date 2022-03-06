75b1561c3691b05e9800d54098d680242b853e06
Eduardo Ugolini <edu.ugolini2@gmail.com>
2022-03-06 18:32:02 -0300

    Add locale param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@
 
 # Base controller for the project
 class ApplicationController < ActionController::API
+  around_action :define_locale
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActionController::ParameterMissing, with: :missing_param
+
+  def define_locale
+    I18n.locale = if I18n.available_locales.include?(params['locale']&.to_sym)
+                    params['locale']
+                  else
+                    I18n.default_locale
+                  end
+    yield
+  end
 
   def not_found
     head :not_found
